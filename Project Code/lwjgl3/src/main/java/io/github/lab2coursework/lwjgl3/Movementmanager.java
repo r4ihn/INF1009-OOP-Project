8fe37df0af742403266468892150caeed309f31f
Lemonades-sch <2501089@sit.singaporetech.edu.sg>
2026-01-27 23:36:36 +0800

    Added Movementmanager

---
+++ Project Code/lwjgl3/src/main/java/io/github/lab2coursework/lwjgl3/Movementmanager.java
@@ -0,0 +1,32 @@
+package io.github.lab2coursework.lwjgl3;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import java.util.ArrayList;
+public class MovementManager {
+
+    private ArrayList<Entity> entities;
+
+    public void rightMovement(Entity entity) {
+        entity.setX(entity.getX() - entity.getSpeed());
+    }
+
+    public void leftMovement(Entity entity) {
+        entity.setX(entity.getX() + entity.getSpeed()); 
+    }
+
+    public void upMovement(Entity entity) {
+        entity.setY(entity.getY() + entity.getSpeed());
+    }
+
+    public void downMovement(Entity entity) {
+        entity.setY(entity.getY() - entity.getSpeed());
+    }
+
+    public void stopMovement(Entity entity) {
+    // Movement management logic to be implemented
+    }
+
+    public void setSpeed(Entity entity, float speed) {
+        entity.setSpeed(speed);
+    }
+}
