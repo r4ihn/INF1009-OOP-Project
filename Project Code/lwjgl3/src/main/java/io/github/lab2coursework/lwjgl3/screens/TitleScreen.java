6bf8d2f195f59b38664ac8f57d83c78836dc00b9
Lemonades-sch <2501089@sit.singaporetech.edu.sg>
2026-02-20 03:44:54 +0800

    Updating TitleScreen's UI

--- Project Code/lwjgl3/src/main/java/io/github/lab2coursework/lwjgl3/screens/TitleScreen.java
+++ Project Code/lwjgl3/src/main/java/io/github/lab2coursework/lwjgl3/screens/TitleScreen.java
@@ -2,6 +2,7 @@ package io.github.lab2coursework.lwjgl3.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import io.github.lab2coursework.lwjgl3.managers.ScreenManager;
 
@@ -23,12 +24,29 @@ public class TitleScreen extends AbstractScreen {
 
     @Override
     protected void draw(float delta) {
-        Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1);
+        // 1. Set background color
+        Gdx.gl.glClearColor(0.08f, 0.08f, 0.12f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         batch.begin();
-        font.draw(batch, "Press ENTER to Start", 350, 300);
-        font.draw(batch, "Press ESCAPE to Exit", 350, 250);
+
+        // 2. Center the Title
+        float screenWidth = Gdx.graphics.getWidth();
+        float screenHeight = Gdx.graphics.getHeight();
+
+        // Scaling the font for the Title
+        font.getData().setScale(1f);
+        font.setColor(Color.CYAN);
+        font.draw(batch, "OOP SIMULATION", screenWidth / 2 - 75, screenHeight - 200);
+
+        // 3. Stylize the Menu Options
+        font.getData().setScale(1f);
+        font.setColor(Color.WHITE);
+        font.draw(batch, ">> Press ENTER to Start <<", screenWidth / 2 - 100, 300);
+
+        font.setColor(Color.GRAY);
+        font.draw(batch, "Press ESCAPE to Exit", screenWidth / 2 - 80, 250);
+
         batch.end();
     }
 }
