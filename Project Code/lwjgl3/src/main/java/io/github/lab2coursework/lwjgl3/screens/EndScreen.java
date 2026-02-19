f826aa4dff26305a9a3ee14d4eb12f51a3865402
r4ihn <122985992+r4ihn@users.noreply.github.com>
2026-02-19 14:51:14 +0800

    Add EndScreen class with restart and return functionality to GameScreen and TitleScreen

--- Project Code/lwjgl3/src/main/java/io/github/lab2coursework/lwjgl3/screens/EndScreen.java
+++ Project Code/lwjgl3/src/main/java/io/github/lab2coursework/lwjgl3/screens/EndScreen.java
@@ -1,4 +1,36 @@
 package lwjgl3.src.main.java.io.github.lab2coursework.lwjgl3.screens;
 
-public class EndScreen {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import io.github.lab2coursework.lwjgl3.ScreenManager;
+import lwjgl3.src.main.java.io.github.lab2coursework.lwjgl3.managers.ScreenManager;
+
+public class EndScreen extends io.github.lab2coursework.lwjgl3.screens.AbstractScreen {
+
+    public EndScreen(ScreenManager screenManager) {
+        super(screenManager);
+    }
+
+    @Override
+    protected void update(float delta) {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.R)) {
+            screenManager.set(new GameScreen(screenManager));
+        }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+            screenManager.set(new TitleScreen(screenManager));
+        }
+    }
+
+    @Override
+    protected void draw(float delta) {
+        Gdx.gl.glClearColor(0.2f, 0.05f, 0.05f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        font.draw(batch, "END SCREEN", 100, 300);
+        font.draw(batch, "Press R to Restart", 100, 260);
+        font.draw(batch, "Press ESC to Title", 100, 230);
+        batch.end();
+    }
 }
