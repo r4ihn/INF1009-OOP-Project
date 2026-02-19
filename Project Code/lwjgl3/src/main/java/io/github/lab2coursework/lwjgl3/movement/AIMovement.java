45f9c819162ac8b430c64032fa0cafd64f52ceec
Lemonades-sch <2501089@sit.singaporetech.edu.sg>
2026-02-20 03:42:26 +0800

    Changing Movement class to Add in IoManager

--- Project Code/lwjgl3/src/main/java/io/github/lab2coursework/lwjgl3/movement/AIMovement.java
+++ Project Code/lwjgl3/src/main/java/io/github/lab2coursework/lwjgl3/movement/AIMovement.java
@@ -10,8 +10,11 @@ public class AIMovement extends Movement {
 
         // Loop when object falls off the bottom of the screen (Y < 0)
         if (entity.getY() < 0) {
-            // Reset it back to the top of the screen - using 1920 x 1080
-            entity.setY(1080);
+            // Reset it back to the top of the screen - using 1280 x 720
+            entity.setY(720);
+            if (entity.getSpeed() < 400f){
+                entity.setSpeed(entity.getSpeed() * 1.5f);
+            }
         }
     }
 }
