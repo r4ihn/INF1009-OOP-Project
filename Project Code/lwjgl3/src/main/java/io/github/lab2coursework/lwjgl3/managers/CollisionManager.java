c8082d49e139357171cd85f0584d7d103f787f0e
zulhiillmi7 <zulzaidan7@gmail.com>
2026-02-21 00:06:50 +0800

    Revert "added collision resolutions for raindrops with shapes"
    This reverts commit 92b1838ebb7aaf8849f1ca53dedf79da62397a1c.

--- Project Code/lwjgl3/src/main/java/io/github/lab2coursework/lwjgl3/managers/CollisionManager.java
+++ Project Code/lwjgl3/src/main/java/io/github/lab2coursework/lwjgl3/managers/CollisionManager.java
@@ -3,7 +3,6 @@ package io.github.lab2coursework.lwjgl3.managers;
 import io.github.lab2coursework.lwjgl3.collision.CollisionRule;
 import io.github.lab2coursework.lwjgl3.collision.KeepInBoundsRule;
 import io.github.lab2coursework.lwjgl3.collision.PhysicsCollisionRule;
-import io.github.lab2coursework.lwjgl3.collision.RaindropCollisionRule;
 import io.github.lab2coursework.lwjgl3.entities.Entity;
 
 import java.util.ArrayList;
@@ -20,9 +19,6 @@ public class CollisionManager {
     public void applyAll(List<Entity> entities, float worldW, float worldH) {
         if (entities == null) return;
 
-        List<CollisionRule> frameRules = new ArrayList<>(rules);
-        frameRules.add(new RaindropCollisionRule(worldW, worldH));
-
         CollisionRule bounds = new KeepInBoundsRule(worldW, worldH);
         for (Entity e : entities) {
             if (bounds.matches(e, null)) bounds.resolve(e, null);
@@ -33,7 +29,7 @@ public class CollisionManager {
                 Entity a = entities.get(i);
                 Entity b = entities.get(j);
 
-                for (CollisionRule rule : frameRules) {
+                for (CollisionRule rule : rules) {
                     if (rule.matches(a, b)) {
                         rule.resolve(a, b);
                     }
